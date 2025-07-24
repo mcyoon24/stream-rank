73eb54040b90d64b28272e58ebc1ca793526afa0
mcyoon24 <mcyoon24@gmail.com>
2025-07-24 15:40:49 -0400

    added rankingpage homepage and notfound

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import logo from './logo.svg';
+import { Outlet } from 'react-router-dom';
 import './App.css';
 import MovieList from './components/MovieList';
+import RankingPage from './components/RankingPage';
 
 function App() {
   return (
     <div>
       <h1>IMDb Ratings for Netflix</h1>
-      <MovieList />
+      <Outlet />  
     </div>
   );
 }
